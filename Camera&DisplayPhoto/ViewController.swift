0092226eca0f0728a22a920f0039e7700643f389
Christa Kulanda <christakulanda@gmail.com>
2021-08-03 13:46:47 -0500

    initial commit

---
+++ Camera&DisplayPhoto/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Camera&DisplayPhoto
+//
+//  Created by Scholar on 8/3/21.
+//
+
+import UIKit
+//comment here! :)
+//another comment 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+
+    @IBAction func takeSelfieTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil )
+    }
+    
+    @IBAction func chooseLibraryTapped(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
